6c03da7ef0198579d965b4565599995e889f54d0
John Michael M. Inoc <johnmichael.inoc@cit.edu>
2025-11-25 21:17:49 +0800

    Analytics frontend

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -31,7 +31,7 @@ export default function Sidebar() {
     { name: "Calendar", path: "/calendar", icon: "/SideBarIcons/Clndr.png" },
     { name: "Study Mode", path: "/flashcards", icon: "/SideBarIcons/StdMd.png" },
     { name: "Library", path: "/library", icon: "/SideBarIcons/Lib.png" },
-    { name: "Analytics", path: "/progress", icon: "/SideBarIcons/Anl.png" },
+    { name: "Analytics", path: "/analytics", icon: "/SideBarIcons/Anl.png" },
     { name: "Music", path: "/music", icon: "/SideBarIcons/Msc.png" },
   ];
   return (
